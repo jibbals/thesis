fd41edf2a929221f7d178c5a830cfe45c690a81b
Jesse Greenslade <bboyjesse@gmail.com>
2018-04-20 12:00:59 +1000

    More of Jenny's comments and citation fixes to citep

--- replace_citet.sh
+++ replace_citet.sh
@@ -15,7 +15,14 @@ fi
 #
 # For testing:
 #echo '\citep{Young,blah203} blah, \citep{Juger}' | sed 's/\\citep{\([^}]*\)}/(\\cite{\1})/g'
+#sed -i 's/\\citep{\([^}]*\)}/(\\cite{\1})/g' $1
 
-sed -i 's/\\citep{\([^}]*\)}/(\\cite{\1})/g' $1
+# Now I want to go back from (\citet{...}) to \citep{...}
 
+# For testing:
 
+echo 'blah blah (\citet{Young,blah203}) blah, (\citet{Juger})'
+echo 'becomes'
+echo 'blah blah (\citet{Young,blah203}) blah, (\citet{Juger})' | sed 's/(\\citet{\([^}]*\)})/\\citep{\1}/g'
+echo 'making copy of input file at check_me.txt'
+sed 's/(\\citet{\([^}]*\)})/\\citep{\1}/g' $1 > check_me.txt
